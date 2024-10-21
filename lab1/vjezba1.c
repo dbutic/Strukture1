89024b4a71d0155d99157cdc5cc4cc153a37940a
dbutic <dbutic9@gmail.com>
2024-10-21 15:32:03 +0200

    vjezba

--- lab1/vjezba1.c
+++ lab1/vjezba1.c
@@ -1,13 +1,8 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-#define MAX_LENGHT 100
-#define MAX_POINTS 100.0
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_LENGTH 100
-#define MAX_POINTS 100.0
+#define MAX_POINTS 100.0 
 
 typedef struct {
     char firstName[MAX_LENGTH];
@@ -18,28 +13,56 @@ typedef struct {
 int countStudents(const char *filename) {
     FILE *file = fopen(filename, "r");
     int studentCount = 0;
-    char buffer[MAX_LENGTH];
+    char firstName[MAX_LENGTH], lastName[MAX_LENGTH];
+    float points;
 
     if (file == NULL) {
         printf("Error opening file!\n");
         return -1;
     }
-    while(!feof(file)){
 
-    }
-
-    while (fgets(buffer, MAX_LENGTH, file) != NULL) {
-        studentCount++;
+    while (!feof(file)) {
+        if (fscanf(file, "%s %s %f", firstName, lastName, &points) == 3) {
+            studentCount++;
+        }
     }
 
     fclose(file);
     return studentCount;
 }
 
+int loadStudents(const char *filename, Student *students, int studentCount) {
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    int i = 0;
+
+    while (!feof(file) && i < studentCount) {
+        if (fscanf(file, "%s %s %f", students[i].firstName, students[i].lastName, &students[i].points) == 3) {
+            i++;
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
+
+void printStudents(Student *students, int studentCount) {
+    printf("Students and their points:\n");
+    for (int i = 0; i < studentCount; i++) {
+        float relativePoints = (students[i].points / MAX_POINTS) * 100.0;
+        printf("%s %s - Absolute points: %.2f, Relative points: %.2f%%\n",
+               students[i].firstName, students[i].lastName, students[i].points, relativePoints);
+    }
+}
+
 int main() {
-    FILE *file;
     Student *students;
-    int studentCount, i;
+    int studentCount;
     const char *filename = "students.txt";
 
     studentCount = countStudents(filename);
@@ -54,28 +77,14 @@ int main() {
         return 1;
     }
 
-    file = fopen(filename, "r");
-    if (file == NULL) {
-        printf("Error opening file!\n");
-        free(students);  
+    if (loadStudents(filename, students, studentCount) != 0) {
+        free(students);
         return 1;
     }
 
-    for (i = 0; i < studentCount; i++) {
-        fscanf(file, "%s %s %f", students[i].firstName, students[i].lastName, &students[i].points);
-    }
-
-
-    fclose(file);
-
-    printf("Students and their points:\n");
-    for (i = 0; i < studentCount; i++) {
-        float relativePoints = (students[i].points / MAX_POINTS) * 100.0;
-        printf("%s %s - Absolute points: %.2f, Relative points: %.2f%%\n",
-               students[i].firstName, students[i].lastName, students[i].points, relativePoints);
-    }
+    printStudents(students, studentCount);
 
     free(students);
-  
+
     return 0;
 }
