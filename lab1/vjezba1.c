7670308125e609d31fbd276a70e131cc64d32425
dbutic <dbutic9@gmail.com>
2024-10-21 15:26:46 +0200

    vjezba

--- lab1/vjezba1.c
+++ lab1/vjezba1.c
@@ -1,17 +1,12 @@
-/*1. Napisati program koji prvo pročita koliko redaka ima datoteka, tj.koliko ima studenata
-zapisanih u datoteci.Nakon toga potrebno je dinamički alocirati prostor za niz struktura
-studenata(ime, prezime, bodovi) i u    čitati iz datoteke sve zapise.Na ekran ispisati ime,
-prezime, apsolutni i relativni broj bodova.
-Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na kolokviju.
-relatvan_br_bodova = br_bodova / max_br_bodova * 100*/
-
-
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
 #define MAX_LENGHT 100
+#define MAX_POINTS 100.0
+
+#define MAX_LENGTH 100
 #define MAX_POINTS 100.0
 
 typedef struct {
